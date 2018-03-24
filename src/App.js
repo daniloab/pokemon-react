104865f5ac0651b9d7193063385d9d81a2301f93
Luís Felipe Souza <luisfmsouza@gmail.com>
2018-03-24 14:37:26 -0300

    Criar as proprieades `searchName` e `pokemon` no estado
    - `searchName` vai ser responsável por guardar o nome do pokemon buscado;
    - `pokemon` vai guardar o valor retornado pela API do PokeAPI

--- src/App.js
+++ src/App.js
@@ -4,18 +4,15 @@ import './App.css';
 import Button from 'react-bootstrap/lib/Button';
 import Col from 'react-bootstrap/lib/Col';
 
-// class PokemonForm extends React.Component {
-
-//   constructor(props){
-//     super(props);
-//     this.state = {value: ''};
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-// }
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchName: "", pokemon: null };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
