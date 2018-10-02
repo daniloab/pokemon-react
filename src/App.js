6595ff54fc2e4a968dce668abe80d4190ad28892
Danilo Assis <danilo.botaro@tribofox.com.br>
2018-10-02 15:19:44 -0300

    Corrigindo exibição dos campos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Col from "react-bootstrap/lib/Col";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { searchName: "", pokemon: null };
+    this.state = { searchName: "", pokemon: null, description: "" };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,7 +15,7 @@ class App extends Component {
 
   handleChange(event) {
     const searchName = event.target.value;
-    this.setState({ searchName: "", pokemon: null });
+    this.setState({ searchName });
   }
 
   async handleSubmit(event) {
@@ -28,27 +28,32 @@ class App extends Component {
 
     const pokemon = await fetchResponse.json();
 
-    this.setState({ pokemon });
+    const fetchResponse2 = await fetch(
+      `https://pokeapi.co/api/v2/characteristic/${pokemon.id}/`
+    )
 
-    console.log(this.state.pokemon);
+    const pokemonInfo = await fetchResponse2.json();
+
+    pokemonInfo.descriptions.map((obj) => {
+      if(obj.language.name == "en")
+        pokemon.description = obj.description
+    })
+
+    this.setState({ pokemon});
   }
 
-  showSection (pokemon) {
+  showSection (pokemon, description) {
     return (
       <div className="app-show-pokemon">
           <div className="poke-result">
               <div className="poke-img">
                   <p>{pokemon.name}</p>
                   <img src={pokemon.sprites.front_shiny_female} />
-                  <span># number - </span><span>description</span>
+                  <span># {pokemon.id} - </span><span>{this.state.description}</span>
               </div>
               <div className="poke-infos">
-                  <span>type1</span>
-                  <span>type2</span>
+                  <span>{pokemon.types[0].type.name}</span>
                   <span>Weight: {pokemon.width}</span>
-                  <ul>
-                      <li>name</li>
-                  </ul>
               </div>
           </div>
       </div>
@@ -57,11 +62,11 @@ class App extends Component {
 
   render() {
     const { pokemon } = this.state;
-    
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo-pokemon" />
           <h1 className="App-title">Find your favorite pokemon!</h1>
         </header>
         <div className="search">
